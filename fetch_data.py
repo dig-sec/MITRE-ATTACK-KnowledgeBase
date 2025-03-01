5510c4d0b105c07560a8d7f637514a499ef58abc
pabi <pabi@dig-sec.com>
2025-03-01 10:12:52 +0100

    Added attack.json and target_pack_generator

--- fetch_data.py
+++ fetch_data.py
@@ -2,16 +2,18 @@ import os
 import requests
 
 # URLs of the files
-json_url = "https://raw.githubusercontent.com/krdmnbrk/AttackRuleMap/main/attack_rule_map.json"
-csv_url = "https://raw.githubusercontent.com/sduff/mitre_attack_csv/main/enterprise-attack.csv"
+attack_rule_map_url = "https://raw.githubusercontent.com/krdmnbrk/AttackRuleMap/main/attack_rule_map.json"
+enterprise_attack_csv_url = "https://raw.githubusercontent.com/sduff/mitre_attack_csv/main/enterprise-attack.csv"
+enterprise_attack_json_url = "https://raw.githubusercontent.com/mitre/cti/refs/heads/master/enterprise-attack/enterprise-attack.json"
 
 # Path to the data folder
-data_folder = os.path.join(os.path.dirname(__file__), 'data')
-os.makedirs(data_folder, exist_ok=True)
+data_directory = os.path.join(os.path.dirname(__file__), 'data')
+os.makedirs(data_directory, exist_ok=True)
 
 # Paths to save the files
-json_file_path = os.path.join(data_folder, 'attack_rule_map.json')
-csv_file_path = os.path.join(data_folder, 'enterprise-attack.csv')
+attack_rule_map_path = os.path.join(data_directory, 'attack_rule_map.json')
+enterprise_attack_csv_path = os.path.join(data_directory, 'enterprise-attack.csv')
+enterprise_attack_json_path = os.path.join(data_directory, 'enterprise-attack.json')
 
 # Function to fetch and save a file
 def fetch_and_save_file(url, file_path):
@@ -26,8 +28,11 @@ def fetch_and_save_file(url, file_path):
     else:
         print(f"Failed to fetch the file from {url}. Status code: {response.status_code}")
 
-# Fetch and save the JSON file
-fetch_and_save_file(json_url, json_file_path)
+# Fetch and save the attack rule map JSON file
+fetch_and_save_file(attack_rule_map_url, attack_rule_map_path)
 
-# Fetch and save the CSV file
-fetch_and_save_file(csv_url, csv_file_path)
+# Fetch and save the enterprise attack CSV file
+fetch_and_save_file(enterprise_attack_csv_url, enterprise_attack_csv_path)
+
+# Fetch and save the enterprise attack JSON file
+fetch_and_save_file(enterprise_attack_json_url, enterprise_attack_json_path)
